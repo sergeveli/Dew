bca8be0cfd04d791eb77b74a8e283ba36e689c92
sergeveli <70241892+sergeveli@users.noreply.github.com>
2021-08-25 16:10:50 -0400

    Backend validateSignup: COMPLETE

--- authenticate-me/backend/routes/api/users.js
+++ authenticate-me/backend/routes/api/users.js
@@ -1,17 +1,39 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors,
+];
+
 // Sign up
 router.post(
   '',
+  validateSignup,
   asyncHandler(async (req, res) => {
-    const { email, password, userName } = req.body;
-    const user = await User.signup({ email, userName, password });
+    const { email, password, username } = req.body;
+    const user = await User.signup({ email, username, password });
 
     await setTokenCookie(res, user);
 
@@ -20,5 +42,6 @@ router.post(
     });
   }),
 );
+
 
 module.exports = router;
