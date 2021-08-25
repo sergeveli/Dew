b3a4eca58e635f1c117551e9989bded1f3c6d7cd
sergeveli <70241892+sergeveli@users.noreply.github.com>
2021-08-25 15:09:48 -0400

    Backend SignUpRoute: COMPLETE

--- authenticate-me/backend/routes/api/users.js
+++ authenticate-me/backend/routes/api/users.js
@@ -1,4 +1,24 @@
-const express = require('express')
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
+
+// Sign up
+router.post(
+  '',
+  asyncHandler(async (req, res) => {
+    const { email, password, userName } = req.body;
+    const user = await User.signup({ email, userName, password });
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user,
+    });
+  }),
+);
 
 module.exports = router;
