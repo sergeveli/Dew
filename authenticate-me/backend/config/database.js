a06aa5c2aec011a88ff86c02d3e2ebbf9d8df3a4
sergeveli <70241892+sergeveli@users.noreply.github.com>
2021-08-25 02:14:22 -0400

    Backend Phase3: COMPLETE

--- authenticate-me/backend/config/database.js
+++ authenticate-me/backend/config/database.js
@@ -1,14 +1,14 @@
 const config = require('./index');
 
 const db = config.db;
-const username = db.username;
+const userName = db.userName;
 const password = db.password;
 const database = db.database;
 const host = db.host;
 
 module.exports = {
   development: {
-    username,
+    userName,
     password,
     database,
     host,
