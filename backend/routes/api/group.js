2cfacf8a560d2adb286525697a48105db96644ad
sergeveli <70241892+sergeveli@users.noreply.github.com>
2021-08-31 14:47:20 -0400

    backend API routes built but need testing

---
+++ backend/routes/api/group.js
@@ -0,0 +1,61 @@
+const { Router } = require('express');
+const express = require('express');
+const expressAsyncHandler = require('express-async-handler');
+const router = express.Router();
+
+const asyncHandler = require('express-async-handler');
+
+const { Group, User, Task } = require('../../db/models');
+
+//(C)CREATE A NEW GROUP 
+router.post('/new',
+    asyncHandler(
+        async (req, res) => {
+
+                userId = req.body.userId
+                taskId = req.body.taskId  
+
+            const newGroup = await Group.create({ 
+                userId,
+                taskId
+            })
+            return await res.json({})
+    }))
+
+//(R)FETCH ALL GROUPS
+router.get('/:id/groups',
+    asyncHandler(
+        async (req, res) => {
+            userId = req.params.id
+            const groups = await Group.findAll(
+                {include: 
+              {
+                model: Task
+              }});
+            return await res.json(groups);
+    })
+);
+
+//(U)EDITING A GROUP,
+router.put('/:groupId',
+    asyncHandler(
+        async(req, res) => {
+            const inputGroup = req.params.task
+            taskId = req.params.taskId
+            const group = await Group.findByPk(groupId)
+            await task.update({taskId})
+            return res.json('')                              
+}))
+
+//(D)DELETE GROUP
+router.delete('/:groupId',
+        asyncHandler(
+            async (req, res) => {
+            groupId = req.params.taskId
+                const group = await Group.findByPk(groupId)
+                await group.destroy(); 
+                    return await res.json('Finito');
+        })
+    )
+
+module.exports = router;
